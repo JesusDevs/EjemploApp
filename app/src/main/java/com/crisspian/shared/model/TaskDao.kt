067b260d4f885429f9d65faeb942104e94e72eec
Leon RO <leon.rodriguez.ore@gmail.com>
2021-01-27 10:51:53 -0300

    aplicación que realizara consulta a una base de datos en kotlin

--- app/src/main/java/com/crisspian/shared/model/TaskDao.kt
+++ app/src/main/java/com/crisspian/shared/model/TaskDao.kt
@@ -1,29 +1,33 @@
 package com.crisspian.shared.model
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 
 @Dao
 interface TaskDao {
+    //metodos o acciones de la base de datos
+    // agregar palabra reservada suspend ,indica al sistema
+    // que estos metodos se ejecutaran asincronos por corutinas , hilo secundario de rutinas
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun createTask(task: Task)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun createTask(task: Task)
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertAllTask(taskList: List<Task>)
+    suspend fun insertAllTask(taskList: List<Task>)
 
     @Update
-    fun updateTask(task: Task)
+    suspend fun updateTask(task: Task)
 
     @Delete
-    fun deleteTask(task: Task)
+    suspend fun deleteTask(task: Task)
 
     @Query("DELETE FROM task_table")
-    fun deleteAllTask()
+   suspend fun deleteAllTask()
 
     @Query("SELECT * FROM task_table WHERE id = :mId")
-    fun getTaskByID(mId: Int): Task
+    //envolver objeto en Live data ,convierte objetos en vistas
+    fun getTaskByID(mId: Int): LiveData<Task>
 
     @Query("SELECT * FROM task_table")
-    fun getAllTask(): List<Task>
+    fun getAllTask(): LiveData<List<Task>>
 
 }
